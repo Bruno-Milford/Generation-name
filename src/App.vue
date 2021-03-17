a9378bf2b715c48145ac863299b5de535995c382
Trynda-LOL <bm33518@gmail.com>
2021-03-17 17:24:35 -0300

    created project and add component ListName

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="slogan" class="text-center">
+      <h1>Name Gator</h1>
+      <br />
+      <h6 class="text-secondary">Gerador de nomes utilizando Vue.js, GraphQL, e Node</h6>
+    </div>
+    <ListName />
+  </div>
+</template>
+
+<script>
+import ListName from './components/ListName'
+
+export default {
+  components: { ListName },
+  name: 'App'
+}
+</script>
+
+<style>
+
+#slogan {
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+
+</style>
---
+++ src/components/ListName.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>Prefixos <span class="badge badge-info">{{ prefixes.length }}</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="prefix in prefixes" :key="prefix">
+                    {{ prefix }}
+                  </li>
+                </ul>
+                <br />
+                <input class="form-control" type="text" placeholder="Digite o prefixo" />
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>Sufixos <span class="badge badge-info">{{ sufixes.length }}</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="sufix in sufixes" :key="sufix">
+                    {{ sufix }}
+                  </li>
+                </ul>
+                <br />
+                <input class="form-control" type="text" placeholder="Digite o sufixo" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css"
+import "font-awesome/css/font-awesome.css"
+
+export default {
+  data() {
+    return {
+      prefixes: ['Air', 'Jet', 'Flight'],
+      sufixes: ['Hub', 'Station', 'Mart']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+#main {
+  background-color: #f1f1f1;
+  padding-top: 30px;
+  padding-bottom: 30px;
+}
+
+</style>
